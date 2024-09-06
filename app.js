df840551a7f8963ccef177ab57d9d704912f506d
krulewski <vinicius.delfino@fatec.sp.gov.br>
2024-09-06 11:06:11 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+function pesquisar() {
+    let section = document.getElementById("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
+
+    // se campoPesquisa for uma string sem nada
+    if (!campoPesquisa) {
+        section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar o nome de um atleta ou esporte </p>"
+        return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+let resultados = "";
+let titulo = "";
+let descricao = "";
+let tags = "";
+// para cada dado dentro da lista de dados
+for (let dado of dados) {
+    titulo = dado.titulo.toLowerCase()
+    descricao = dado.descricao.toLowerCase()
+    tags= dado.tags.toLocaleLowerCase()
+    // se titulo includes campoPesquisa
+    if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa) ) {
+        resultados += `
+        <div class="item-resultado">
+            <h2>
+                <a href="#" target="_blank">${dado.titulo}</a>
+            </h2>
+            <p class="descricao-meta">${dado.descricao}</p>
+            <a href="${dado.link}" target="_blank">Mais informações</a>
+        </div>
+    `;
+    }
+
+  
+}
+
+if (!resultados) {
+    resultados = "<p>Nada foi encontrado</p>"
+}
+
+section.innerHTML = resultados
+}
+
+
+
+
+
---
+++ dados.js
@@ -0,0 +1,23 @@
+let dados = [
+    {
+        titulo : "Rayssa Leal",
+        descricao : "Rayssa Leal, a Fada do Skate, é uma skatista profissional brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos de idade, ela já coleciona títulos importantes e inspira milhões de jovens.",
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
+    }, 
+
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de Paris 2024, conquistando a medalha de ouro na categoria +78kg. Sua técnica e força a tornaram uma das principais atletas do judô mundial.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judô judo"
+      },
+      
+      {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta artística brasileira que conquistou o coração dos brasileiros nos Jogos Olímpicos de Tóquio 2020 e Paris 2024. Sua versatilidade e carisma a tornaram um dos maiores nomes da ginástica mundial.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginastica ouro"
+      }
+];
+
